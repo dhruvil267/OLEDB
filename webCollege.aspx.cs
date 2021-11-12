a8e76431b643c5a0f3702850db48609867600f8b
Dhruvil Patel <patel2dhruvil202@gmail.com>
2021-11-12 14:06:36 -0500

    Add project files.

---
+++ webCollege.aspx.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OLEDB
+{
+
+    public partial class webCollege : System.Web.UI.Page
+    {
+        static OleDbConnection myCon;
+        OleDbCommand myCmd;
+        OleDbDataReader rdSchool;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+
+
+
+                myCon = new OleDbConnection(@"Provider = Microsoft.Jet.OLEDB.4.0; Data Source =" + Server.MapPath("~/App_Data/College2.mdb"));
+                myCon.Open();
+
+                myCmd = new OleDbCommand("select Refschool, Title from Schools order by title", myCon);
+                rdSchool= myCmd.ExecuteReader();
+                radlistSchool.DataTextField = "Title";
+                
+                radlistSchool.DataValueField = "Refschool";
+                radlistSchool.DataSource = rdSchool;
+                radlistSchool.DataBind();
+                panCourse.Visible = false;
+                panPrograms.Visible = false;
+
+               
+            }
+        }
+
+        protected void radlistSchool_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            myCmd = new OleDbCommand("Select refProgram,title from programs where referschool=@ref", myCon);
+            myCmd.Parameters.AddWithValue("ref", radlistSchool.SelectedItem.Value);
+            rdSchool = myCmd.ExecuteReader();
+            radlstPrograms.DataTextField = "Title";
+
+            radlstPrograms.DataValueField = "RefProgram";
+            radlstPrograms.DataSource = rdSchool;
+            radlstPrograms.DataBind();
+            panPrograms.Visible = true;
+
+
+        }
+
+        protected void radlstPrograms_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            myCmd = new OleDbCommand("select RefCourse, [Number] from Courses where referProgram=@refpr", myCon);
+            myCmd.Parameters.AddWithValue("refpr", radlstPrograms.SelectedItem.Value);
+
+            rdSchool = myCmd.ExecuteReader();
+            chklstCourses.DataTextField = "Number";
+
+            chklstCourses.DataValueField = "RefCourse";
+            chklstCourses.DataSource = rdSchool;
+            chklstCourses.DataBind();
+            panCourse.Visible = true;
+
+        }
+
+        protected void chklstCourses_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(chklstCourses.SelectedIndex > -1)
+            {
+                string sql = "select studentname as [Names], Birthdate as [Birth Dates], Telephone, Average,Email from Students Where ReferCourse="+chklstCourses.SelectedItem.Value;
+                foreach(ListItem item in chklstCourses.Items)
+                {
+                    if (item.Selected)
+                    {
+                        sql += " or ReferCourse=" + item.Value;
+                    }
+                }
+                sql += " order by studentname";
+                myCmd = new OleDbCommand(sql, myCon);
+                rdSchool = myCmd.ExecuteReader();
+                gridStudents.DataSource = rdSchool;
+                gridStudents.DataBind();
+
+            }
+            else
+            {
+                gridStudents.DataSource = null;
+                gridStudents.DataBind();
+            }
+
+
+
+        }
+
+        protected void gridStudents_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ webDataReader.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.OleDb;
+
+namespace OLEDB
+{
+    public partial class webDataReader : System.Web.UI.Page
+    {
+        static OleDbConnection myCon;
+        OleDbCommand myCmd;
+        OleDbDataReader rdStudents;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+
+
+
+                myCon = new OleDbConnection(@"Provider = Microsoft.Jet.OLEDB.4.0; Data Source =" + Server.MapPath("~/App_Data/College.mdb"));
+                myCon.Open();
+
+                myCmd = new OleDbCommand("select RefCourse, [Number] from Courses order by [Number]", myCon);
+                rdStudents = myCmd.ExecuteReader();
+
+                lstCourse.DataTextField = "Number";
+                lstCourse.DataValueField = "RefCourse";
+                lstCourse.DataSource = rdStudents;
+                lstCourse.DataBind();
+
+                string sql = "select * from courses where teacher =@teach and duration  <@dur";
+                OleDbCommand myCmdTest = new OleDbCommand(sql, myCon);
+                OleDbParameter myPar = new OleDbParameter("teach", "Houria Houmel");
+                myPar.DbType = System.Data.DbType.String;
+                myCmdTest.Parameters.Add(myPar);
+                myCmdTest.Parameters.AddWithValue("dur", 50);
+                OleDbDataReader rdTest = myCmdTest.ExecuteReader();
+                gridTest.DataSource = rdTest;
+                gridTest.DataBind();
+            }
+
+
+
+        }
+
+        protected void lstCourse_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            myCmd = new OleDbCommand("Select * from courses where refcourse=@ref", myCon);
+            myCmd.Parameters.AddWithValue("ref", lstCourse.SelectedItem.Value);
+            rdStudents = myCmd.ExecuteReader();
+            if (rdStudents.Read())
+            {
+                txtNumber.Text = rdStudents["Number"].ToString();
+                txtTitle.Text = rdStudents["Title"].ToString();
+                txtDuration.Text = rdStudents["Duration"].ToString();
+                txtTeacher.Text = rdStudents["Teacher"].ToString();
+                txtDescription.Text = rdStudents["Description"].ToString();
+
+            }
+            rdStudents.Close();
+            myCmd.CommandText = "Select StudentName as [Names],BirthDate as [Birth Dates], Telephone,Average,Email from Students where refercourse=@ref";
+            rdStudents = myCmd.ExecuteReader();
+            gridResult.DataSource = rdStudents;
+            gridResult.DataBind();
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int refc = Convert.ToInt32(lstCourse.SelectedItem.Value);
+            string sql = "update courses set duration=@dur,teacher=@tea,description=@des where refcourse=@courseid";
+            OleDbCommand myCmd = new OleDbCommand(sql, myCon);
+            myCmd.Parameters.AddWithValue("dur",Convert.ToInt32(txtDuration.Text));
+            myCmd.Parameters.AddWithValue("tea", txtTeacher.Text);
+            myCmd.Parameters.AddWithValue("des",txtDescription.Text);
+            myCmd.Parameters.AddWithValue("courseid", refc);
+            int result = myCmd.ExecuteNonQuery();
+            Response.Write("<script>alert(\"Updated\");</script>");
+        }
+    }
+}
